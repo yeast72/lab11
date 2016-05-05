2774791a3500d4207eff9fcac776a52483e70730
jbrucker <jebrucker@gmail.com>
2016-05-05 12:49:32 +0700

    minor edit

--- src/student/StudentApp.java
+++ src/student/StudentApp.java
@@ -1,9 +1,18 @@
 package student;
-
 import java.util.List;
 
+/**
+ * Display reminders of students having a birthday soon.
+ * @author you
+ */
 public class StudentApp {
 
+	/**
+	 * Print the names (and birthdays) of students having a birtday in the
+	 * specified month.
+	 * @param students list of students
+	 * @param month the month to use in selecting bithdays
+	 */
 	public void filterAndPrint( List<Student> students, int month ) {
 		for(Student s : students ) {
 			if (s.getBirthdate().getMonthValue() == month)
